4a5618ca026013bad7cd867a330c7e1cbd2fe440
ingridfti <indi.15@live.com>
2025-04-12 20:38:28 -0300

    Cad_exames_atualizada
    atualização do mapa bioquimica

--- cadastroExames.php
+++ cadastroExames.php
@@ -4,93 +4,102 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- getbootstrap.com > include via CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>aula 09/04</title>
+    <title>Cadastro de Exames Bioquímicos</title>
+    <style>
+    td {
+        vertical-align: middle;
+    }
+    </style>
 </head>
 
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <!-- Aqui vai o menu -->
-                <?php
-                //include "menu.php";
-                require "menu.php";
-            ?>
-            </div>
-            <div class="col-5 border bg-light p-5">
-                <h1 class="display-5">Cadastro de exames</h1>
-                <form action="dados.php" method="get">
-                    <div class="mb-3">
-                        <label for="registro" class="form-label">Número do registro</label>
-                        <input type="text" class="form-control" name="registro" id="registro">
-                    </div>
-                    <div class="mb-3">
-                        <label for="nome_paciente" class="form-label">Nome do paciente</label>
-                        <input type="text" class="form-control" name="nome_paciente" id="nome_paciente">
-                    </div>
-                    <div class="mb-3">
-                        <label for="entrada" class="form-label">Entrada</label>
-                        <input type="text" class="form-control" name="entrada" id="entrada">
-                    </div>
-                    <div class="mb-3">
-                        <label for="data_laudo" class="form-label">Data prevista para entrega do laudo</label>
-                        <input type="text" class="form-control" name="data_laudo" id="data_laudo">
-                    </div>
-                    <div class="mb-3">
-                        <label for="data_exame" class="form-label">Data e hora de realização do exame</label>
-                        <input type="text" class="form-control" name="data_exame" id="data_exame">
-                    </div>
-                    <div class="mb-3">
-                        <p>Exames para qual laboratório?</p>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="bioq" value="bioquimica"
-                                id="flexCheckDefault">
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Bioquímica
-                            </label>
+<body class="bg-light">
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-10 border bg-white p-5 shadow rounded">
+                <h2 class="mb-4 text-center">Cadastro de Exames Bioquímicos</h2>
+
+                <form action="salvar_exame.php" method="POST">
+                    <!-- Informações principais -->
+                    <div class="row mb-4">
+                        <div class="col-md-4">
+                            <label for="numero_registro" class="form-label">Nº do Registro</label>
+                            <input type="text" class="form-control" name="numero_registro" id="numero_registro"
+                                required>
                         </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="hemato" value="hematologia"
-                                id="flexCheckChecked">
-                            <label class="form-check-label" for="flexCheckChecked">
-                                Hematologia
-                            </label>
+                        <div class="col-md-4">
+                            <label for="nome_paciente" class="form-label">Nome do Paciente</label>
+                            <input type="text" class="form-control" name="nome_paciente" id="nome_paciente">
                         </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="imuno" value="imunologia"
-                                id="flexCheckChecked">
-                            <label class="form-check-label" for="flexCheckChecked">
-                                Imunologia
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="micro" value="microbiologia"
-                                id="flexCheckChecked">
-                            <label class="form-check-label" for="flexCheckChecked">
-                                Microbiologia
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="parasito" value="parasitologia"
-                                id="flexCheckChecked">
-                            <label class="form-check-label" for="flexCheckChecked">
-                                Parasitologia
-                            </label>
+                        <div class="col-md-4">
+                            <label for="data_laudo" class="form-label">Data prevista para entrega do laudo</label>
+                            <input type="date" class="form-control" name="data_laudo" id="data_laudo">
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-success">Salvar via GET</button>
-                </form>
-            </div>
-            <div class="col p-3 mt-2">
-                <!-- Solicitação via POST para formulario.php-->
-                <form action="dados.php" method="post">
-                    <input type="text" name="oi">
-                    <button type="submit" class="btn btn-danger">
-                        Solictar dados.php via POST
-                    </button>
+
+                    <div class="mb-4">
+                        <label for="data_exame" class="form-label">Data e hora da realização do exame</label>
+                        <input type="datetime-local" class="form-control" name="data_exame" id="data_exame">
+                    </div>
+
+                    <!-- Tabela de exames -->
+                    <div class="table-responsive mb-4">
+                        <table class="table table-bordered align-middle text-center">
+                            <thead class="table-secondary">
+                                <tr>
+                                    <th>Exame</th>
+                                    <th>Valor Absoluto</th>
+                                    <th>Valor de Referência</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $exames = [
+                                    'bilirrubina_total' => ['Bilirrubina Total (mg/dL)', '0,2 – 1,2 mg/dL'],
+                                    'bilirrubina_direta' => ['Bilirrubina Direta (mg/dL)', '0,0 – 0,2 mg/dL'],
+                                    'proteina_total' => ['Proteína Total', ''],
+                                    'tgo' => ['TGO - Transaminase Glutamico Oxalacética (U/L)', '5 – 34 U/L'],
+                                    'tgp' => ['TGP - Transaminase Glutamico Piruvica (U/L)', 'Masculino: 21 – 72 U/L<br>Feminino: 9 – 52 U/L'],
+                                    'gama_gt' => ['Gama GT - Glutamiltransferase (U/L)', 'Masculino: 15 – 73 U/L<br>Feminino: 12 – 43 U/L'],
+                                    'fosfatase_alcalina' => ['Fosfatase Alcalina (U/L)', '38 – 126 U/L'],
+                                    'creatinina' => ['Creatinina (mg/dL)', 'Masculino: 0,70 – 1,25 mg/dL<br>Feminino: 0,57 – 1,11 mg/dL'],
+                                    'glicose' => ['Glicose (mg/dL)', '75 – 99 mg/dL'],
+                                    'colesterol_total' => ['Colesterol Total (mg/dL)', 'Adultos (acima de 20 anos): menor que 190 mg/dL<br>Crianças e adolescentes (menores de 20 anos): menor que 170 mg/dL'],
+                                    'triglicerideos' => ['Triglicerídeos (mg/dL)', 'Adultos (acima de 20 anos): menor que 150 mg/dL<br>Crianças de 0 a 9 anos: menor que 75 mg/dL<br>Crianças e adolescentes de 10 a 19 anos: menor que 90 mg/dL'],
+                                    'ureia' => ['Ureia (mg/dL)', ''],
+                                    'acido_urico' => ['Ácido Úrico (mg/dL)', ''],
+                                    'pcr' => ['PCR - Proteína C Reativa (mg/dL)', 'Inferior a 1,0 mg/dL'],
+                                    'calcio' => ['Cálcio', ''],
+                                    'ldh' => ['LDH', '']
+                                ];                                
+
+                                foreach ($exames as $name => [$label, $referencia]) {
+                                    echo "<tr>";
+                                    echo "<td class='text-start'><label for='{$name}'>{$label}</label></td>";
+                                    echo "<td><input type='number' step='0.01' class='form-control' name='{$name}' id='{$name}'></td>";
+                                    echo "<td class='text-muted text-start'>{$referencia}</td>";
+                                    echo "</tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <!-- Responsáveis -->
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="responsavel_exame" class="form-label">Responsável pelo exame</label>
+                            <input type="text" class="form-control" name="responsavel_exame" id="responsavel_exame">
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="preceptor_responsavel" class="form-label">Preceptor responsável</label>
+                            <input type="text" class="form-control" name="preceptor_responsavel"
+                                id="preceptor_responsavel">
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-success w-100">Salvar Exames</button>
                 </form>
             </div>
         </div>
